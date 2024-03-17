a4e84cb2230f684d5df0f4277c39a9dcb4c21feb
James <Michael.James02@mmc.com>
2024-03-16 20:56:33 -0400

    Added a CartController.

---
+++ WeatherWatch.Web/Controllers/CartController.cs
@@ -0,0 +1,28 @@
+﻿using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace WeatherWatch.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly DaprClient daprClient;
+
+        public CartController(
+            DaprClient daprClient)
+        {
+            this.daprClient = daprClient;
+        }
+
+        public IActionResult Index()
+        {
+            zipCode = Guid.NewGuid().ToString();
+
+            //var result = daprClient.GetStateAsync<string>("statestore-web", cartId.ToString());
+
+            daprClient.SaveStateAsync("statestore-web", zipCode, zipCode);
+
+            return View();
+        }
+    }
+}
